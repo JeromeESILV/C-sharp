53c0b58c8618d5ca77af803e64a1b7d9d4264c29
JeromeESILV <77071019+JeromeESILV@users.noreply.github.com>
2025-02-04 23:34:57 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //définir la méthode SaisieNombre();
+        //chaque exercice sera définie sous forme d'une méthode static
+
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(
+                    "Menu :\n"
+                        + "Exercice 1 : Draw A Line & A Matrix ...\n"
+                        + "Exercice 2 : Draw A Matrix With a Diagonal ...\n"
+                        + "Exercice 3 : Multiplication Table...\n"
+                        + "Exercice 4 : Inverse Word...\n"
+                        + "\n"
+                        + "Chose Desired Exercise "
+                );
+                string exo = Console.ReadLine();
+                switch (exo)
+                {
+                    case "1":
+                        Exo1();
+                        break;
+                    case "2":
+                        Exo2();
+                        break;
+                    case "3":
+                        Exo3();
+                        break;
+                    case "4":
+                        Exo4();
+                        break;
+                    default:
+                        Console.WriteLine("No exercise selected");
+                        break;
+                }
+                Console.WriteLine("Chose Another Exercise Or Press ESC Then Enter To Leave");
+                cki = Console.ReadKey();
+            } while (cki.Key != ConsoleKey.Escape);
+
+            Console.Read();
+        }
+
+        static void Exo1()
+        {
+            Console.WriteLine("Input Line Size Desired...\n");
+            int LineDim = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < LineDim; i++)
+            {
+                Console.Write("X");
+            }
+            Console.Write("\n");
+
+            Console.WriteLine("Input Symbol For Matrix...");
+            string MatSym = Console.ReadLine();
+            Console.WriteLine("Input Dimention For Matrix...");
+            int MatDim = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < MatDim; i++)
+            {
+                for (int j = 0; j < MatDim; j++)
+                {
+                    Console.Write(MatSym);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void Exo2()
+        {
+            Console.WriteLine("Input Symbol For Matrix...");
+            string MatSym = Console.ReadLine();
+            Console.WriteLine("Input Diagonal Symbol For Matrix...");
+            string MatDiag = Console.ReadLine();
+            Console.WriteLine("Input Dimention For Matrix...");
+            int MatDim = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input The Direction Of The Diagonal For Matrix(M or D)...");
+            string MatDir = Console.ReadLine();
+
+            if (MatDir == "D")
+            {
+                for (int i = 0; i < MatDim; i++)
+                {
+                    for (int j = 0; j < MatDim; j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.Write(MatDiag);
+                        }
+                        else
+                        {
+                            Console.Write(MatSym);
+                        }
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+            if (MatDir == "M")
+            {
+                for (int i = 0; i < MatDim; i++)
+                {
+                    for (int j = 0; j < MatDim; j++)
+                    {
+                        if (i + j == MatDim - 1)
+                        {
+                            Console.Write(MatDiag);
+                        }
+                        else
+                        {
+                            Console.Write(MatSym);
+                        }
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+
+        static void Exo3()
+        {
+            Console.WriteLine("Enter A Whole Number...");
+            int Num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < Num; i++)
+            {
+                for (int j = 0; j < Num; j++)
+                {
+                    int mult = (i + 1) * (j + 1);
+                    for (
+                        int k = 0;
+                        k < ((Num * Num).ToString().Length - mult.ToString().Length + 1);
+                        k++
+                    )
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(mult);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void Exo4()
+        {
+            Console.WriteLine("Enter A String To Be Inversed...");
+            string str = Console.ReadLine();
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Console.Write(str[i]);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp2 // Note: actual namespace depends on the project name.
+{
+    static class Program
+    {
+        public static ArrayList arr = new ArrayList();
+        static void Main(string[] args)
+        {
+            
+            ConsoleKeyInfo cki;
+            do {
+                Console.Clear();
+                Console.WriteLine("Chose An Exercise From The Following...");
+                Console.WriteLine("Exercise 1: Set Array Values... " + "\nExercise 2: Display Array Set..." + "\nExercise 3: Inverse Array... " + "\nExercise 4: Sort Array..." + "\nExercise 5: Is Array Sorted?..." + "\nExercise 6: Search Value..." + "\nExercise 7: Count Value...");
+
+                string exo = Console.ReadLine();
+                switch (exo)
+                {
+                    case "1":
+                        Exo1();
+                        break;
+                    case "2":
+                        Exo2();
+                        break;
+                    case "3":
+                        Exo3();
+                        break;
+                    case "4":
+                        Exo4();
+                        break;
+                    case "5":
+                        Console.WriteLine(Exo5());                        
+                        break;
+                    case "6":
+                        Console.WriteLine(Exo6());
+                        break;
+                    case "7":
+                        Exo7();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("No exercise selected");
+                        break;
+                }
+                Console.WriteLine("Chose Another Exercise Or Press ESC Then Enter To Leave");
+                cki = Console.ReadKey();
+            } while (cki.Key != ConsoleKey.Escape);
+
+            Console.Read();
+        }
+
+        static void Exo1() {
+            ConsoleKeyInfo cki;
+            arr.Clear();
+            Console.WriteLine("Insert Numbers Into Array..." + "\n ESC + Enter When done...");
+            int i = 0;
+
+            do
+            {
+                Console.Write("[{0}] ", i++);
+                
+                arr.Add(int.Parse(Console.ReadLine()));
+                cki = Console.ReadKey();
+                Console.WriteLine("");
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+        static void Exo2() {
+            int[] tmpArr = arr.ToArray(typeof(int)) as int[];
+            Console.WriteLine("Your Array : " + "[{0}]", string.Join(", ", tmpArr));
+        }
+        static void Exo3() {
+
+            int[] tmpArr = arr.ToArray(typeof(int)) as int[];
+            Array.Reverse(tmpArr);
+            Console.WriteLine("Inversed Array : " + "[{0}]", string.Join(", ", tmpArr));
+        }
+        static void Exo4() {
+            int[] tmpArr = arr.ToArray(typeof(int)) as int[];
+            Array.Sort(tmpArr);
+            Console.WriteLine("Inversed Array : " + "[{0}]", string.Join(", ", tmpArr));
+        }
+         static bool Exo5()
+         {
+            int[] tmpArr = arr.ToArray(typeof(int)) as int[];
+
+            for (int i = 1; i < tmpArr.Length; i++)
+            {
+                if (tmpArr[i - 1] > tmpArr[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+         }
+        static bool Exo6() {
+            int[] tmpArr = arr.ToArray(typeof(int)) as int[];
+
+            Console.WriteLine("Set Value To Search For...");
+            int info = int.Parse(Console.ReadLine());
+
+            foreach(int i in tmpArr)
+            {
+                if(i == info)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Exo7() {
+            int[] tmpArr = arr.ToArray(typeof(int)) as int[];
+
+            Console.WriteLine("Set Value To Count ...");
+            int info = int.Parse(Console.ReadLine());
+            int count = 0;
+            foreach (int i in tmpArr)
+            {
+                if (i == info)
+                {
+                    count++; ;
+                }
+            }
+            Console.WriteLine("{0} Found {1} Times...", info, count);
+        }
+
+    }
+}
